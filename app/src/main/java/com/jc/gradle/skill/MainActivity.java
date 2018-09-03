72f7c421e1804e0291f468653eb8cb9d8b4e2a7d
Super <aohanyao@gmail.com>
2018-09-03 19:48:38 +0800

    增加注释配置
    增加注释配置

--- app/src/main/java/com/jc/gradle/skill/MainActivity.java
+++ app/src/main/java/com/jc/gradle/skill/MainActivity.java
@@ -3,12 +3,14 @@ package com.jc.gradle.skill;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.jc.gradle.core.lib.TestUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        com.jc.gradle.core.lib.TestUtils.pr
+        TestUtils.printTest();
     }
 }
